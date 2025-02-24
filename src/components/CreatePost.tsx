daec6ff9c49252efc3ed50e74f5eb7371bbcd3b9
Oct <octiduarte@hotmail.com>
2025-02-24 18:10:13 -0300

    t

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,6 +1,13 @@
-"use client"
-import React, { useState } from 'react'
-import {useUser} from "@clerk/nextjs"
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Textarea } from "./ui/textarea";
+import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
 const CreatePost = () => {
     const {user} = useUser()
     const [content, setContent] = useState("")
@@ -10,9 +17,24 @@ const CreatePost = () => {
 
 
   return (
-    <div>
-      CreatePost
-    </div>
+    <Card className='mb-6'>
+      <CardContent className="pt-6">
+        <div className='space-y-4'>
+        <div className="flex space-x-4">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src={user?.imageUrl || "/avatar.png"} />
+            </Avatar>
+            <Textarea
+              placeholder="What's on your mind?"
+              className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              disabled={isPosting}
+            />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
